951b8cb85da1f54dab025343596343fa2eccc293
hystech <120216176+hystech@users.noreply.github.com>
2022-12-11 01:14:21 +0800

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,39 @@
+<?php>
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@hystch.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+		"User Email: $visitor_email.\n".
+		"Subject: $subject.\n".
+		"User Message: $message .\n";
+
+$to = 'huiyuen97@hotmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contactsv.html");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
